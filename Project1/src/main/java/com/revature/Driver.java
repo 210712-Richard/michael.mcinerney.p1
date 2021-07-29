1d7180d368fcdbf8ea39992f03eb079c3f8e2d4a
mcinerneym <mcinerney.michael.t@gmail.com>
2021-07-28 20:16:36 -0700

    Added the User fields and methods. Filled out ApprovalStatus and UserType enums.

--- Project1/src/main/java/com/revature/Driver.java
+++ Project1/src/main/java/com/revature/Driver.java
@@ -3,6 +3,21 @@ package com.revature;
 public class Driver {
 
 	public static void main(String[] args) {
+		setupDatabase();
+		launchJavalin();
+	}
+	
+	/**
+	 * Used to setup the initial tables and populate with default rows
+	 */
+	private static void setupDatabase() {
+		
+	}
+	
+	/**
+	 * Used to launch Javalin to start the REST API
+	 */
+	private static void launchJavalin() {
 		
 	}
 
