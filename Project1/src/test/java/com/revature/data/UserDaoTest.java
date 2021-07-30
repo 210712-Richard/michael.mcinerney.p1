2dc4b88cde35354bc327008ad5f71cd3fc12871a
mcinerneym <mcinerney.michael.t@gmail.com>
2021-07-30 07:14:59 -0700

    Test stubs added for UserDaoTest

--- Project1/src/test/java/com/revature/data/UserDaoTest.java
+++ Project1/src/test/java/com/revature/data/UserDaoTest.java
@@ -1,5 +1,50 @@
 package com.revature.data;
 
-public class UserDaoTest {
+import org.junit.jupiter.api.Test;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+
+public class UserDaoTest {
+	UserDao userDao = new UserDaoImpl();
+	CqlSession session = null;
+	
+	@Test
+	public void testAddUser() {
+		
+	}
+
+	@Test
+	public void testGetUserByIDValid() {
+		
+	}
+
+	@Test
+	public void testGetUserByIDInvalid() {
+		
+	}
+
+	@Test
+	public void testGetUserByNameValid() {
+		
+	}
+
+	@Test
+	public void testGetUserByNameInvalid() {
+		
+	}
+
+	@Test
+	public void testGetUserByNamePasswordValid() {
+		
+	}
+
+	@Test
+	public void testGetUserByNamePasswordInvalid() {
+		
+	}
+
+	@Test
+	public void testUpdateUser() {
+		
+	}
 }
