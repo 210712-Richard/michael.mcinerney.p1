ba4f9f109252e50075ab4539ac21aaf0c16b37cf
mcinerneym <mcinerney.michael.t@gmail.com>
2021-07-30 07:07:48 -0700

    Added the method stubs for the UserDao and DeptDao

--- Project1/src/main/java/com/revature/data/DepartmentDao.java
+++ Project1/src/main/java/com/revature/data/DepartmentDao.java
@@ -1,5 +1,20 @@
 package com.revature.data;
 
-public interface DepartmentDao {
+import com.revature.beans.Department;
+import com.revature.beans.DepartmentName;
 
+public interface DepartmentDao {
+	
+	/**
+	 * Get the department based on the department name
+	 * @param deptName The name of the department
+	 * @return The department with the same name
+	 */
+	Department getDepartment(DepartmentName deptName);
+	
+	/**
+	 * Add a new department to the database
+	 * @param dept The department being added to the database
+	 */
+	void createDepartment(Department dept);
 }
--- Project1/src/main/java/com/revature/data/DepartmentDaoImpl.java
+++ Project1/src/main/java/com/revature/data/DepartmentDaoImpl.java
@@ -1,8 +1,22 @@
 package com.revature.data;
 
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.revature.beans.Department;
+import com.revature.beans.DepartmentName;
 import com.revature.factory.TraceLog;
+import com.revature.util.CassandraUtil;
 
 @TraceLog
 public class DepartmentDaoImpl implements DepartmentDao{
+	private CqlSession session = CassandraUtil.getInstance().getSession();
+
+	@Override
+	public Department getDepartment(DepartmentName deptName) {
+		return null;
+	}
+
+	@Override
+	public void createDepartment(Department dept) {
+	}
 
 }
