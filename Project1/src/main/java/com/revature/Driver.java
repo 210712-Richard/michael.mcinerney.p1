5be4dd6db8fcc8dad017ee2477cad5ff955e3ae0
mcinerneym <mcinerney.michael.t@gmail.com>
2021-08-02 09:30:37 -0700

    Added the methods for logging in in controller and service.

--- Project1/src/main/java/com/revature/Driver.java
+++ Project1/src/main/java/com/revature/Driver.java
@@ -1,5 +1,8 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.revature.controllers.RequestController;
 import com.revature.controllers.RequestControllerImpl;
 import com.revature.controllers.UserController;
@@ -7,11 +10,14 @@ import com.revature.controllers.UserControllerImpl;
 import com.revature.factory.BeanFactory;
 import com.revature.util.DatabaseCreator;
 
+import io.javalin.Javalin;
+import io.javalin.plugin.json.JavalinJackson;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		setupDatabase();
-//		launchJavalin();
+//		setupDatabase();
+		launchJavalin();
 		
 	}
 	
@@ -40,12 +46,34 @@ public class Driver {
 	 * Used to launch Javalin to start the REST API
 	 */
 	private static void launchJavalin() {
+		
+		//Change mapper settings to make sure LocalDateTime objects show correctly
+		ObjectMapper jackson = new ObjectMapper();
+		jackson.registerModule(new JavaTimeModule());
+		jackson.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		JavalinJackson.configure(jackson);
+		
+		//Start Javalin to process HTTP requests
+		Javalin app = Javalin.create().start(8080);
+		
+		//The controllers for users and requests.
 		UserController userControl = (UserController) BeanFactory.getFactory().getObject(UserController.class,
 				UserControllerImpl.class);
 		RequestController reqControl = (RequestController) BeanFactory.getFactory().getObject(RequestController.class,
 				RequestControllerImpl.class);
+		
+		//As a user, I can login
+		app.get("/users", userControl::login);
+		
+		//As a user, I can logout
+		
+		//As an employee, I can create a reimbursement request.
+		
+		//As an employee, I can cancel my reimbursement request.
+		
+		//As an employee, I can upload the final grade/presentation for the reimbursement request. 
+
 	}
-	
 
 
 }
--- Project1/src/main/java/com/revature/controllers/UserControllerImpl.java
+++ Project1/src/main/java/com/revature/controllers/UserControllerImpl.java
@@ -8,9 +8,21 @@ import com.revature.factory.TraceLog;
 import com.revature.services.UserService;
 import com.revature.services.UserServiceImpl;
 
+import io.javalin.http.Context;
+
 @TraceLog
 public class UserControllerImpl implements UserController {
+	
+	//The service for handling User objects
 	UserService userService = (UserService) BeanFactory.getFactory().getObject(UserService.class,
 			UserServiceImpl.class);
+	
+	//Log
 	private static Logger log = LogManager.getLogger(UserControllerImpl.class);
+	
+	
+	@Override
+	public void login(Context ctx) {
+		
+	}
 }
