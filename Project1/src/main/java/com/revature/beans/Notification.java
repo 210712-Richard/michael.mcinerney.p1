8df26435d408bfbf6742614a371b6f78a76b45ef
mcinerneym <mcinerney.michael.t@gmail.com>
2021-08-08 15:50:00 -0700

    Minor fix

--- Project1/src/main/java/com/revature/beans/Notification.java
+++ Project1/src/main/java/com/revature/beans/Notification.java
@@ -6,82 +6,53 @@ import java.util.UUID;
 
 public class Notification {
 	private String username;
-	private UUID id;
+	private UUID requestId;
 	private LocalDateTime notificationTime;
 	private String message;
-	private UUID requestId;
-	
-	
+
 	public Notification(String username, String message, UUID requestId) {
 		this.username = username;
-		this.id = UUID.randomUUID();
 		this.message = message;
 		this.requestId = requestId;
 		this.notificationTime = LocalDateTime.now();
 	}
 
-
 	public String getUsername() {
 		return username;
 	}
-
-	@Override
-	public String toString() {
-		return "Notification [username=" + username + ", id=" + id + ", notificationTime=" + notificationTime
-				+ ", message=" + message + ", requestId=" + requestId + "]";
-	}
-
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
-	public UUID getId() {
-		return id;
-	}
-
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-
 	public LocalDateTime getNotificationTime() {
 		return notificationTime;
 	}
-
 
 	public void setNotificationTime(LocalDateTime notificationTime) {
 		this.notificationTime = notificationTime;
 	}
 
-
 	public String getMessage() {
 		return message;
 	}
-
 
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
-
 	public UUID getRequestId() {
 		return requestId;
 	}
-
 
 	public void setRequestId(UUID requestId) {
 		this.requestId = requestId;
 	}
 
-
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, message, notificationTime, requestId, username);
+		return Objects.hash(message, notificationTime, requestId, username);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -92,13 +63,14 @@ public class Notification {
 		if (getClass() != obj.getClass())
 			return false;
 		Notification other = (Notification) obj;
-		return Objects.equals(id, other.id) && Objects.equals(message, other.message)
-				&& Objects.equals(notificationTime, other.notificationTime)
+		return Objects.equals(message, other.message) && Objects.equals(notificationTime, other.notificationTime)
 				&& Objects.equals(requestId, other.requestId) && Objects.equals(username, other.username);
 	}
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "Notification [username=" + username + ", requestId=" + requestId + ", notificationTime="
+				+ notificationTime + ", message=" + message + "]";
+	}
 
 }
